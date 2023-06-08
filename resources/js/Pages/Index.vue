40370bbef670ab4a2949ed8063102bb1d8b29306
MadeAditya02 <imadeaditya4@gmail.com>
2023-06-08 20:24:49 +0800

    Initialize project

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['title', 'posts'])
+</script>
+<template>
+  <h1 class="text-4xl font-semibold">{{ title }}</h1>
+  <div style="margin-top: 1rem;">
+    <ul>
+      <li v-for="post in posts">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
